7f9187fcdbaaf595cfffb58fa98acff7796fd64e
Bruna Nunes <nunesbrunaa@gmail.com>
2025-07-28 20:02:49 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+
+const clickButton = document.getElementById('click');
+const rootElement = document.documentElement;
+const h1Element = document.querySelector('h1');
+
+const lightTheme = {
+  '--background-color': '#Efe9e1',
+  '--text-color': '#121212',
+  '--button-background-color': '#E6A4B4',
+  '--button-text':'Dark ',
+  '--h1-text': 'Welcome to Light Theme!😵'
+};
+
+const darkTheme = {
+  '--background-color': '#330066',
+  '--text-color': '#f2e6ff',
+  '--button-background-color': '#f2e6ff',
+  '--button-text':'Light',
+  '--h1-text': 'Welcome to Dark Theme!😉'  
+};
+
+let isDarkMode = false;
+
+clickButton.addEventListener('click', function() {
+  isDarkMode = !isDarkMode;
+  isDarkMode ? changeTheme(darkTheme) : changeTheme(lightTheme);
+});
+
+function changeTheme(theme) {
+  for (let prop in theme) {
+    rootElement.style.setProperty(prop, theme[prop]);
+  }
+  // Atualiza o texto do botão e do h1 conforme o tema
+  clickButton.textContent = theme['--button-text'];
+  h1Element.textContent = theme['--h1-text'];
+}
